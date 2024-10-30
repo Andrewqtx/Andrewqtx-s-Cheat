e3d1750effe9b9df27e58c98454f22a39469b031
Andrewqtx <Andrewqtx@ANDREWQTX-PC>
2024-10-30 18:15:36 +0300

    Добавьте файлы проекта.

---
+++ Offsets.hpp
@@ -0,0 +1,27 @@
+namespace OW {
+    namespace offset {
+
+        // Base
+
+        static constexpr auto Address_viewmatrix_base = 0x38095F0;
+        static constexpr auto Address_viewmatrix_base_test = 0x3EDDBB8;
+        static constexpr auto Address_entity_base = 0x37F49C0;
+        static constexpr auto offset_viewmatrix_ptr = 0x7E0;
+        static constexpr auto offset_viewmatrix_xor_key = 0x6B5C14CA4D03DA3E;
+        static constexpr auto HeapManager = 0x38C1E30;
+        static constexpr auto HeapManager_Var = 0x38A6B33;
+        static constexpr auto HeapManager_Key = 0xAC82250EE001802C;
+        static constexpr auto HeapManager_Pointer = 0x160;
+        static constexpr auto changefov = 0x396E7E8;
+        static constexpr auto SensitivePtr = 0x2064;
+
+        static constexpr auto VisFN = 0x7AA2F2;
+        static constexpr auto VisRead = 0x38A6E40;
+        static constexpr auto Vis_Key1 = 0x72E97F66EFD12519;
+        static constexpr auto Vis_Key2 = 0x72E97F66EFD12519;
+
+        static constexpr auto OutlineFN = 0x7B2FA2;
+        static constexpr auto OutlineRead = 0x38A6E40;
+        static constexpr auto Outline_Key = 0x3C0AA484DAE5234Di64;
+    }
+}
